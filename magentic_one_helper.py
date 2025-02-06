5e21cf391641d7b0c414e5e7c2bef1f0d2d4b30a
Michal@MacBook <michal.marusan@gmail.com>
2025-02-06 16:32:16 +0100

    update to gpt-4o-2024-11-20

--- magentic_one_helper.py
+++ magentic_one_helper.py
@@ -101,7 +101,7 @@ class MagenticOneHelper:
             self.session_id = session_id
 
         self.client = AzureOpenAIChatCompletionClient(
-            model="gpt-4o-2024-08-06",
+            model="gpt-4o-2024-11-20",
             azure_deployment="gpt-4o",
             api_version="2024-06-01",
             azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
